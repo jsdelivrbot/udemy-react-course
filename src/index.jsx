061ef6414d5ae086ffce2bc573c2bcc0ed2ee4d9
Mike Weeden <weeden.mike@gmail.com>
2017-04-02 14:16:44 -0400

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+//Youtube API key
+const API_KEY = 'AIzaSyDGWBRSpTCIb81ub8SSVu76oQN4gmvvyxY';
+
+
+
+// Create a new component. This component should produce html
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    //Perform a search via the youtube API
+    YTSearch({
+      key: API_KEY,
+      term: 'h3h3productions'
+    }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Put the component into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));

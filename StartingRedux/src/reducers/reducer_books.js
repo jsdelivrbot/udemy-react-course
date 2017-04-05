87bc05eb2531c6cd0bf64a3197359bc4c5db424c
Mike W <weeden.mike@gmail.com>
2017-04-04 20:41:30 -0400

    Adding # of pages to each book

--- StartingRedux/src/reducers/reducer_books.js
+++ StartingRedux/src/reducers/reducer_books.js
@@ -1,11 +1,15 @@
 export default function() {
   return [{
-    title: 'Javascript: The Good Parts'
+    title: 'Javascript: The Good Parts',
+    pages: 150
   }, {
-    title: 'Harry Potter'
+    title: 'Harry Potter',
+    pages: 159
   }, {
-    title: 'The Dark Tower'
+    title: 'The Dark Tower',
+    pages: 330
   }, {
-    title: 'Clean Code'
+    title: 'Clean Code',
+    pages: 150
   }]
 }

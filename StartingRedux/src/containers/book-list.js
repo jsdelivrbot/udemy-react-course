199b858ade7bc33b5bd053ead480033a4f08ec89
Mike W <weeden.mike@gmail.com>
2017-04-04 20:13:14 -0400

    Adding action creators to BookList container.

--- StartingRedux/src/containers/book-list.js
+++ StartingRedux/src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
   renderList() {
@@ -27,4 +29,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this func will be bound to props on bookList container.
+function mapDispatchToProps(dispatch) {
+  //Whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+//Promote booklist from cmp to container. It needs to know about this
+//dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);

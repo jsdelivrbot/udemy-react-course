5d38dd698c4e4f839380622c24d8021fbef17e28
Mike W <weeden.mike@gmail.com>
2017-04-08 19:10:12 -0400

    Adding axios and react-promise to the project. Hooking up react promise by applying it as a middleware in index.js

--- App 4/src/index.js
+++ App 4/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import promise from 'redux-promise';
 
 import routes from './routes';
 import reducers from './reducers';
 
 const API_KEY = 'TWlrZWZyaWdnaW53ZWVkZW4';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>

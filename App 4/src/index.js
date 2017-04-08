82d6b4760148b3c14914120e4cf322de1610606f
Mike W <weeden.mike@gmail.com>
2017-04-08 19:05:42 -0400

    Added router to the project. Added index route to show a fake list of blog posts.

--- App 4/src/index.js
+++ App 4/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
 const API_KEY = 'TWlrZWZyaWdnaW53ZWVkZW4';
@@ -12,6 +13,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));

0fa82fb96e99a5432d9b0d7bc2ade7afdf281a0e
Mike Weeden <weeden.mike@gmail.com>
2017-04-08 17:52:03 -0400

    Adding Fourth App.

---
+++ App 4/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const API_KEY = 'TWlrZWZyaWdnaW53ZWVkZW4';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));

34b89698327a93b9121d1dda41a409a91fadbc06
Mike Weeden <weeden.mike@gmail.com>
2017-04-08 12:01:02 -0400

    Making our charts into a functional component. Added Styling.

--- App 3/src/containers/weather-list.js
+++ App 3/src/containers/weather-list.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import WeatherChart from '../components/weather-chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const weatherData = cityData.list;
+    const temp = weatherData.map(weather => weather.main.temp);
+    const humidity = weatherData.map(weather => weather.main.humidity);
+    const pressure = weatherData.map(weather => weather.main.pressure);
+
     return (
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
+        <td><WeatherChart data={temp} color="#404040" units="K" /></td>
+        <td><WeatherChart data={humidity} color="#404040" units="hPa" /></td>
+        <td><WeatherChart data={pressure} color="#404040" units="%" /></td>
       </tr>
     );
   }
@@ -16,9 +25,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>

7599020de277a2ad5c0fc7b7f048a79b5951d623
Mike Weeden <weeden.mike@gmail.com>
2017-04-14 14:30:50 -0400

    Hooked up post submission and some minor validation

--- App 4/src/components/post-create.js
+++ App 4/src/components/post-create.js
@@ -1,13 +1,50 @@
 import React, { Component} from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostCreate extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    console.log(title);
     return (
-      <div>
-        FORM
-      </div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 };
 
-export default PostCreate;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  return errors;
+}
+
+//connect: first arg is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: first is config, second is mapStateTOProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostsCreateForm',
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, {createPost})(PostCreate);

46cce461e83cd498fdfde430b743a82d50e0c456
Mike Weeden <weeden.mike@gmail.com>
2017-04-14 17:47:24 -0400

    Adding support for Viewing a single post, and deleting a post.

--- App 4/src/routes.js
+++ App 4/src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts-index';
 import PostCreate from './components/post-create';
+import PostShow from './components/post-show';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/create" component={PostCreate} />
+    <Route path="posts/:id" component={PostShow} />
   </Route>
 );
